861452e5eafc6e69deab2c901c84c9947e360016
Purusottam Mohapatra <35234210+purusottam11@users.noreply.github.com>
2026-06-30 16:35:31 +0530

    Add: first-missing-positive - submission-1

---
+++ Data Structures & Algorithms/first-missing-positive/submission-1.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+
+        for (int i : nums) {
+            if (i > 0) {
+                set.add(i);
+            }
+        }
+
+        for (int i = 1; i <= nums.length; i++) {
+            if (!set.contains(i)) {
+                return i;
+            }
+        }
+        return nums.length + 1;
+    }
+}
